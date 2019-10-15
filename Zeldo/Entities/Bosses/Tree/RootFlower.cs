56a934557f35649019b26152347b311ced259d1c
grimelios <grimelios@gmail.com>
2019-10-15 02:47:04 -0500

    Adding some code for the tree boss

---
+++ Zeldo/Entities/Bosses/Tree/RootFlower.cs
@@ -0,0 +1,27 @@
+﻿using Engine.Shapes._3D;
+using Newtonsoft.Json.Linq;
+using Zeldo.Entities.Core;
+
+namespace Zeldo.Entities.Bosses.Tree
+{
+	public class RootFlower : LivingEntity
+	{
+		public RootFlower() : base(EntityGroups.Boss)
+		{
+		}
+
+		public override void Initialize(Scene scene, JToken data)
+		{
+			var model = CreateModel(scene, "Bosses/RootFlower.obj");
+			var bounds = model.Mesh.Bounds;
+
+			CreateSensor(scene, new Sphere(bounds.x / 2), SensorGroups.Target);
+
+			base.Initialize(scene, data);
+		}
+
+		protected override void OnDeath()
+		{
+		}
+	}
+}
---
+++ Zeldo/Entities/Bosses/Tree/TreeBoss.cs
@@ -0,0 +1,12 @@
+﻿using Zeldo.Entities.Core;
+
+namespace Zeldo.Entities.Bosses.Tree
+{
+	// TODO: Should this class extend Boss? It doesn't actually use a traditional health bar.
+	public class TreeBoss : LivingEntity
+	{
+		public TreeBoss() : base(EntityGroups.Boss)
+		{
+		}
+	}
+}
